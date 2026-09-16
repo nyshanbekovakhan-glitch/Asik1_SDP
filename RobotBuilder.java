ce07c2725e6234a7a288724b8be62468d2d1e113
nyshanbekovakhan-glitch <nyshanbekova.khan@mail.ru>
2026-09-16 09:46:36 +0500

    Add files via upload

---
+++ RobotBuilder.java
@@ -0,0 +1,10 @@
+package robot;
+
+public interface RobotBuilder {
+    RobotBuilder setName(String name);
+    RobotBuilder setRobotType(String robotType);
+    RobotBuilder setProcessor(String processor);
+    RobotBuilder setBattery(int battery);
+    RobotBuilder setSensors(String sensors);
+    RobotBuilder setAI(boolean hasAI);
+}
